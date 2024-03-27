e3943ee793d4ed862f9533470892af169fe8dd3a
alxmart <alxmartinez@gmail.com>
2024-03-27 17:56:52 -0300

    Added ViewBinding configs and fun display to show text when clicked.

--- app/src/main/java/com/luizafmartinez/at05_layoutsstylesthemes/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/at05_layoutsstylesthemes/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.luizafmartinez.at05_layoutsstylesthemes
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.at05_layoutsstylesthemes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun display(view: View) {
+        binding.info.text="Android Application Development ,Android Security Essentials and Monetize Android Applications"
     }
 }
